b90646e065b3996c7995801de17f9ce4424375bd
Ilja Heitlager <iheitlager@schubergphilis.com>
2014-12-30 00:25:37 +0100

    better install/upgrade

--- brew/install.sh
+++ brew/install.sh
@@ -5,6 +5,7 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+PACKAGES=( brew-cask )
 # Check for Homebrew
 if test ! $(which brew)
 then
@@ -16,6 +17,11 @@ fi
 
 # Install homebrew packages
 # brew install grc coreutils spark
-brew install brew-cask
+for package in $PACKAGES; do
+    if ! brew install $package
+    then
+        brew upgrade $package
+    fi
+done
 
 exit 0
