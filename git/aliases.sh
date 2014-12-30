87f18b3df1c5f055b24499157043abcf4b1ab817
Ilja Heitlager <iheitlager@schubergphilis.com>
2014-12-30 08:14:24 +0100

    anothor alias

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,5 +1,7 @@
 alias gall='git add --all'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gp='git push origin'
+alias gch='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
