c8771df5d24c07e078f84f8f4e713de597427eb3
Ilja Heitlager <ilja@heitlager.com>
2014-12-30 09:45:52 +0100

    git completions added, fixed brew

--- brew/install.sh
+++ brew/install.sh
@@ -5,7 +5,8 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-PACKAGES=( brew-cask )
+PACKAGES=( brew-cask bash-completion)
+
 # Check for Homebrew
 if test ! $(which brew)
 then
@@ -17,7 +18,7 @@ fi
 
 # Install homebrew packages
 # brew install grc coreutils spark
-for package in $PACKAGES; do
+for package in ${PACKAGES[@]}; do
     if ! brew install $package
     then
         brew upgrade $package
