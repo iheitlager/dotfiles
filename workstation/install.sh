1bdd6e2f777c14a8af60ae4b7ea4713473199790
Ilja Heitlager <iheitlager@schubergphilis.com>
2015-01-01 18:57:27 +0100

    copying some expert functions

--- workstation/install.sh
+++ workstation/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Lot's of examples taken from https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+
 source "$(dirname "$0")/bash_env"
 
 # Create a set of folders
@@ -7,11 +9,35 @@ if [ ! -d $WORK_DIR ]; then
     echo "  Creating an empty working directory for your git repos"
     mkdir $WORK_DIR
 fi
+# I prefer to have a persistent TEMP dir next to $TMPDIR and ~/.Trash
 if [ ! -d ~/tmp ]; then
     echo "  Creating an empty temp dir"
     mkdir ~/tmp
 fi
 
+############################################################################
+# Terminal & iTerm2
+############################################################################
 
-# Setup iTerm2
+TERM_PROFILE='Workstation';
+CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
+if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
+    open "$(dirname "$0")/${TERM_PROFILE}.terminal";
+    sleep 1; # Wait a bit to make sure the theme is loaded
+    defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
+    defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
+fi;
+
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+############################################################################
+# Google Chrome
+############################################################################
+
+# Disable the all too sensitive backswipe
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+# Use the system-native print preview dialog
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults write com.google.Chrome.canary DisablePrintPreview -bool true
