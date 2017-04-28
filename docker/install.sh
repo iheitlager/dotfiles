211de9ce008561b401d1b41292691500eb582d56
Ilja Heitlager <ilja@heitlager.com>
2017-04-28 07:57:36 +0200

    fixing startup docker-compose

--- docker/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# tab completion for docker
-curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o $(brew --prefix)/etc/bash_completion.d/docker-compose
-
